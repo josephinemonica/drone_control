3313e07ead445b39d98aaadd23fc0036f8f50620
Josephine Monica <josephinemonica09@gmail.com>
2017-09-11 16:41:50 +0800

    add the basic skeleton for plugins (revolute_control_plugin)

---
+++ src/plugins/build/CMakeFiles/1DOF_control_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1DOF_control_plugin.dir/1DOF_control_plugin.cc.o"
+  "lib1DOF_control_plugin.pdb"
+  "lib1DOF_control_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1DOF_control_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/plugins/build/CMakeFiles/revolute_control_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/revolute_control_plugin.dir/revolute_control_plugin.cc.o"
+  "librevolute_control_plugin.pdb"
+  "librevolute_control_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/revolute_control_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
